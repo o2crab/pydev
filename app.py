c1c8847e78c7b9c241c93785b9e6280273dc5fd2
o2crab <bio12kbb@gmail.com>
2022-02-07 09:09:54 +0900

    modify app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!!!'
+    DATABASE_URL = os.environ['DATABASE_URL']
+    return DATABASE_URL
+    #return 'Hello, World!!!'
 
 if __name__ == '__main__':
     app.run()
