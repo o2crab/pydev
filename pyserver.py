200a8c071d0726b9fee21fbb6be2e9fa27e33067
o2crab <bio12kbb@gmail.com>
2022-02-09 21:56:29 +0900

    major change

---
+++ pyserver.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__, static_folder='.', static_url_path='')
+
+@app.route('/')
+def home():
+    return app.send_static_file('index.html')
+
+@app.route('/entry/<q_id>')
+def echo(q_id):
+    return q_id
+
+
+app.run(debug=True)
